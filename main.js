e97ccd0a7ef33682451b9d010334c2c4f3a50bbe
onlymx13 <37835222+onlymx13@users.noreply.github.com>
2018-03-28 08:29:22 -0400

    Totally remade. May be done, may be ruined.

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 var main=function(){
+var add3Arrays=function(array1,array2,array3){
+	if(length(array1)+length(array2)+length(array3)!=3*length(array1)){
+	return null;
+	}
+	var sum=new Array(length(array1));
+for(var i=0;i<length(array1);i++){
+	sum[i]=array1[i]+array2[i];
+}
+for(i=0;i<length(array3);i++){
+	sum[i]=sum[i]+array3[i];
+}
+}
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
 var colorStrings=["BLUE","RED","BLACK","MAGENTA","GREEN","ORANGE","BROWN","NAVY","LTBLUE","YELLOW","WHITE","LTGRAY","MEDGRAY","GRAY","DARKGRAY"];
 var colorChars=["1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+var outputString="";
 var red=[0,255,0,255,0,255,128,0,128,255,255,200,150,100,50];
 var green=[0,0,0,0,255,99,33,0,255,255,255,200,150,100,50];
 var blue=[255,0,0,255,0,0,33,50,255,255,255,200,150,100,50];
 var img = document.getElementById('myImage');
-  var redDiffs=new Array(256);
-  var greenDiffs=new Array(256);
-  var blueDiffs=new Array(256);
-  var mycanvas = document.getElementById('mycanvas');
-  mycanvas.width = img.width;
-  mycanvas.height = img.height
-  var context = mycanvas.getContext('2d');
-  context.drawImage(img, 0, 0, img.width, img.height);
-  for(var x=1;x<=16;x++){
-    for(var y=1;y<=16;y++){
-      var pixelData = context.getImageData(x, y, 1, 1).data;
-      document.getElementById("output").innerHTML="x="+x.toString()+" y="+y.toString()+" pD="+pixelData.toString();
-	  redDiffs[16*x+y-17]=red.map( function(value){
-		  return Math.abs(value-pixelData[0]);
-	  });
-	  greenDiffs[16*x+y-17]=green.map( function(value) {
-		  return Math.abs(value-pixelData[1]); 
-	  });
-	  blueDiffs[16*x+y-17]=blue.map( function(value) {
-		  return Math.abs(value-pixelData[2]);
-	  });
-    }
-  }
-  var totalDiffs=new Array(256);
-  totalDiffs.fill([]);
-  for(var pos=0;pos<256;pos++) {
-	  for(var pos2=0;pos2<15;pos2++){
-  totalDiffs[pos][pos2]=redDiffs[pos][pos2]+greenDiffs[pos][pos2]+blueDiffs[pos][pos2];
-	  }
-  }
-  for (var pos=0;pos<256;pos++){
-	  totalDiffs[pos]
-	  }
-  alert(totalDiffs.toString());	
+var redDiffs=new Array(256);
+var greenDiffs=new Array(256);
+var blueDiffs=new Array(256);
+var mycanvas = document.getElementById('mycanvas');
+mycanvas.width = img.width;
+mycanvas.height = img.height
+var context = mycanvas.getContext('2d');
+context.drawImage(img, 0, 0, img.width, img.height);
+//Read each pixel and make that an array
+//Pixel1=[r1,g1,b1,a1]...
+for(var xInPic=1;xInPic<=16;xInPic++){
+	for(var yInPic=1;yInPic<=16;yInPic++){
+		var pixelData = context.getImageData(xInPic, yInPic, 1, 1).data;
+		//Now do red-ri:
+		var redDiffs=Math.abs(red.map(x => x-pixelData[0]));
+		var greenDiffs=Math.abs(green.map(x => x-pixelData[1]));
+		var blueDiffs=Math.abs(blue.map(x => x-pixelData[2]));
+		var totalDiffs=add3Arrays(redDiffs,greenDiffs,blueDiffs);
+		outputString=outputString+colorChars[totalDiffs.indexOf(totalDiffs.min)];
+		
+}	}
+document.getElementById("output").innerHTML=outputString;
 }
