35912c04b4bfda71eb5a86abf635509005591984
onlymx13 <37835222+onlymx13@users.noreply.github.com>
2018-03-30 07:26:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ var red=[0,255,0,255,0,255,182,0,128,255,255,225,200,150,100];
 var green=[0,0,0,0,221,133,47,0,255,255,255,225,200,150,100];
 var blue=[255,0,0,255,0,51,47,102,255,0,255,225,200,150,100];
 var colorChars=["1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+var reverseArray=function(array){
+return [reverse(array[0]),reverse(array[1]),reverse(array[2]),reverse(array[3]),reverse(array[4]),reverse(array[5]),reverse(array[6]),reverse(array[7]),reverse(array[8]),reverse(array[9]),reverse(array[10]),reverse(array[11]),reverse(array[12]),reverse(array[13]),reverse(array[14]),reverse(array[15])];
+}
 var setup=function(){
 for (var i=0;i<15;i++){
 var div=document.getElementById("div"+(i+1).toString());
@@ -73,9 +76,6 @@ for(var i=0;i<16;i++){
 	newOutputArray[i]=substr(array[0],i)+substr(array[1],i)+substr(array[2],i)+substr(array[3],i)+substr(array[4],i)+substr(array[5],i)+substr(array[6],i)+substr(array[7],i)+substr(array[8],i)+substr(array[9],i)+substr(array[10],i)+substr(array[11],i)+substr(array[12],i)+substr(array[13],i)+substr(array[14],i)+substr(array[15],i);
 }
 return newOutputArray;
-}
-var reverseArray=function(array){
-return [reverse(array[0]),reverse(array[1]),reverse(array[2]),reverse(array[3]),reverse(array[4]),reverse(array[5]),reverse(array[6]),reverse(array[7]),reverse(array[8]),reverse(array[9]),reverse(array[10]),reverse(array[11]),reverse(array[12]),reverse(array[13]),reverse(array[14]),reverse(array[15])];
 }
 document.getElementById("output").innerHTML=':DCS\n"'+(reverseArray(rotateArray(outputArray))).join("");
 },false);
